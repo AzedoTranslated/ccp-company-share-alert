52f37933a0ef716e64742c5ed9565b216a987642
Azedo <118608174+AzedoTranslated@users.noreply.github.com>
2023-05-16 10:33:57 +1000

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+print("Version 1.0")
+time.sleep(1)
+print("No copyright laws apply with this creation, If you wan't to work on this yourself you can do so at https://github.com/AzedoTranslated/ccp-company-share-alert, or you can copy the code and put in in your own project and make it better maybe, I don't care")
+time.sleep(1)
+print()
+for i in range(1000000000000000000000000000000):
+  request=input("Platform or game developer: ")
+  time.sleep(1)
+  if request == "discord":
+   print("WARNING: A company called 'tencent' owns 30% of this company, tencent is almost completly controled by the ccp")
+  Else = 1
+  print()
+  if request == "funcom":
+   print("WARNING: A company called 'tencent' owns 100% of this company, tencent is almost completly controled by the ccp")
+  Else = 1
+  print()
+  if request == "riot games":
+   print("WARNING: A company called 'tencent' owns 100% of this company, tencent is almost completly controled by the ccp")
+  Else = 1
+  print()
+  if request == "tiktok":
+   print("WARNING: A company called bytedance has complete control over this platform, bytedance is completly controlled by the CCP")
+   Else = 1
+   print()
+  if request == "Klei entertainment":
+   print("WARNING: A company called 'tencent' owns 100% of this company, tencent is almost completly controled by the ccp")
+   Else = 1
+   print()
+  if Else == 1:
+   print("This company may be safe, but this is only a new project, if you know it has a ccp owned shareholder, then let me know at azedotranslated@proton.me or create a pull request on our github at https://github.com/AzedoTranslated/ccp-company-share-alert")
+   print()
+   time.sleep(0.5)
+   print("")
